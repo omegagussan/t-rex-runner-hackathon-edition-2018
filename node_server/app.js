293aec257b6a1e46ec3b8f27b4309a1134763ca0
Dan Bergh Johnsson <dan.bergh.johnsson@omegapoint.se>
2018-10-12 19:12:51 +0200

    Restructured code for reinforcement in phases

--- node_server/app.js
+++ node_server/app.js
@@ -6,8 +6,8 @@ var fs = require('fs');
 var convnetjs = require("convnetjs-ts");
 
 const brainOpt = { start_learn_threshold: 100 };
-const brain = new convnetjs.deepqlearn.Brain(1, 2, brainOpt); // 3 inputs, 2 possible outputs (0,1)
-const state = [0];
+const brain = new convnetjs.deepqlearn.Brain(1, 2, brainOpt); // 1 inputs, 2 possible outputs (0,1)
+const state = [false]; // the window we look in - anyone there?
 
 app.listen(3000);
 
@@ -47,31 +47,47 @@ io.on('connection', function (socket) {
 
     // Pulls a frame from the game
     socket.on('frame', function ({frame_id, frame_data}) {
-        console.log('I got a frame, number: ' + frame_id + ' base64 to image it to look at it!');
         //console.log(frame_data)
     });
 
 
-    // läs från "frame"
-    // detektera om det kommer något nära dinosauren
+    //console.log('I got a frame, number: ' + frame_id + ' base64 to image it to look at it!');
 
 
-    // high bird = 50
-    // mid bird = 75
-    // high cactus = 90
-    // low cactus = 105
-    // low bird = ?
-
-    // Gör samma sak, fast genom att kika på bilden
     socket.on('state', function({frame_id, status, obstacles, score, high_score}) {
+        // läs från "frame"
+        // detektera om det kommer något nära dinosauren
 
-        console.log(status);
 
-
+        // high bird = 50
+        // mid bird = 75
+        // high cactus = 90
+        // low cactus = 105
+        // low bird = ?
+        console.log('I got a frame, number: ' + frame_id);
+        // update state
+        state[0] = obstacleExistsAndIsInWindow(obstacles[0]);
+        console.log("state: " + state);
+        // Select action based on state
         const action = brain.forward(state);
-        action === 1 ? socket.emit('jump', frame_id+1) : console.log("0");
-        brain.backward(obstacleExistsAndIsInWindow(obstacles[0]));
-        state[obstacleExistsAndIsInWindow(obstacles[0])];
+
+        switch (action) {
+            case 0:
+                console.log("NOP");
+                break;
+            case 1:
+                console.log("JUMP");
+                socket.emit('jump', frame_id+1);
+                break;
+            default:
+                console.log("Sorry, we are out of " + expr + ".");
+        }
+
+
+        var reward = 0;
+        // reward += status*0.01; // penalty for action
+        // update reward with crash penalty
+        brain.backward(0);
         // Feedback
         // status crashed ->    academy.addRewardToAgent(agent, -1.0)
         // stuats still alive ->     academy.addRewardToAgent(agent, 1.0)
@@ -86,6 +102,7 @@ io.on('connection', function (socket) {
 
         if (status === 'CRASHED'){
             console.log('GAME OVER!');
+            socket.emit('start');
         }
         console.log('your score is: ' +  score + ' highscore : ' + high_score);
         if ((((status !== 'JUMPING')
