eb48a4d9678a703c6c6fc029d78b88a521572d1d
Gustav Sandström <gussan@omeagapoint.se>
2018-10-12 14:52:21 +0200

    conda env export > environment.yml

--- node_server/app.js
+++ node_server/app.js
@@ -3,6 +3,7 @@ console.log("starting...");
 var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var fs = require('fs');
+let lwip = require('lwip');
 
 app.listen(3000);
 
@@ -19,23 +20,39 @@ function handler (req, res) {
         });
 }
 
+function writeToFile(buffer_data) {
+  fs.writeFile('lastestFrame.png', buffer_data, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+}
+
+function fromBase64(frame_data) {
+  let data = frame_data.replace(/^data:image\/\w+;base64,/, "");
+  return new Buffer(data, 'base64');
+}
+
 io.on('connection', function (socket) {
     socket.emit('start');
 
     socket.on('frame', function ({frame_id, frame_data}) {
-        console.log(frame_data)
-        console.log('I got a frame, number: ' + frame_id + ' base64 to image it to look at it!');
+      let image = fromBase64(frame_data);
+      writeToFile(image);
+
+
+
     });
-    socket.on('state', function({frame_id, status, obstacles, score, high_score}) {
-        console.log(status);
-        if (status === 'CRASHED'){
-            console.log('GAME OVER!');
-        }
-        console.log('your score is: ' +  score + ' highscore : ' + high_score);
-        if (((status !== 'JUMPING') && (obstacles.length > 0) && ((obstacles[0].xPos + obstacles[0].width) <= 160))){ //TODO: dont jump for high birds. Consider pace of dino to time jump!
-            socket.emit('jump', frame_id+1);
-        } //TODO: make logic for ducking.
-    });
+
+    // socket.on('state', function({frame_id, status, obstacles, score, high_score}) {
+    //     console.log(status);
+    //     if (status === 'CRASHED'){
+    //         console.log('GAME OVER!');
+    //     }
+    //     console.log('your score is: ' +  score + ' highscore : ' + high_score);
+    //     if (((status !== 'JUMPING') && (obstacles.length > 0) && ((obstacles[0].xPos + obstacles[0].width) <= 160))){ //TODO: dont jump for high birds. Consider pace of dino to time jump!
+    //         socket.emit('jump', frame_id+1);
+    //     } //TODO: make logic for ducking.
+    // });
 });
 
 
