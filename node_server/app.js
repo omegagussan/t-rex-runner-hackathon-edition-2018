fffd672959c978ab10fcf1accba586e430552f50
Dan Bergh Johnsson <dan.bergh.johnsson@omegapoint.se>
2018-10-12 14:59:50 +0200

    A plan for looking ahead by inspecting frames

--- node_server/app.js
+++ node_server/app.js
@@ -22,17 +22,29 @@ function handler (req, res) {
 io.on('connection', function (socket) {
     socket.emit('start');
 
+    // Pulls a frame from the game
     socket.on('frame', function ({frame_id, frame_data}) {
         console.log(frame_data)
         console.log('I got a frame, number: ' + frame_id + ' base64 to image it to look at it!');
     });
+
+
+    // läs från "frame"
+    // detektera om det kommer något nära dinosauren
+
+
+    // Gör samma sak, fast genom att kika på bilden
     socket.on('state', function({frame_id, status, obstacles, score, high_score}) {
         console.log(status);
         if (status === 'CRASHED'){
             console.log('GAME OVER!');
         }
         console.log('your score is: ' +  score + ' highscore : ' + high_score);
-        if (((status !== 'JUMPING') && (obstacles.length > 0) && ((obstacles[0].xPos + obstacles[0].width) <= 160))){ //TODO: dont jump for high birds. Consider pace of dino to time jump!
+        if (((status !== 'JUMPING')
+            && (obstacles.length > 0) // Finns ett hinder
+            && ((obstacles[0].xPos + obstacles[0].width) // hindrets högerkatnt
+                <= 160) // "är nära"
+         )){ //TODO: dont jump for high birds. Consider pace of dino to time jump!
             socket.emit('jump', frame_id+1);
         } //TODO: make logic for ducking.
     });
