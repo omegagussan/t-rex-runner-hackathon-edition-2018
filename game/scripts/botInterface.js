fdfbbbe1fffbc9d1d2cb4d1b57ccf26f62587f3b
Gustav Sandström <gussan@omeagapoint.se>
2018-10-12 19:35:44 +0200

    2nd bot fix

--- game/scripts/botInterface.js
+++ game/scripts/botInterface.js
@@ -1,84 +1,89 @@
+let is_emitted_gameover = false;
 //helper functions
 function copyCanvasWithRescale(oldCanvas, downsample_factor) {
-    if (typeof downsample_factor !==  "number") {
-        throw "downsample_factor is not a integer";
-    }
-    let inverse_downsample_factor = 1/downsample_factor;
+  if (typeof downsample_factor !==  "number") {
+    throw "downsample_factor is not a integer";
+  }
+  let inverse_downsample_factor = 1/downsample_factor;
 
-    //create a new canvas
-    let newCanvas = document.createElement('canvas');
-    let newContext = newCanvas.getContext('2d');
+  //create a new canvas
+  let newCanvas = document.createElement('canvas');
+  let newContext = newCanvas.getContext('2d');
 
-    //set dimensions
-    newCanvas.width = oldCanvas.width/downsample_factor;
-    newCanvas.height = oldCanvas.height/downsample_factor;
+  //set dimensions
+  newCanvas.width = oldCanvas.width/downsample_factor;
+  newCanvas.height = oldCanvas.height/downsample_factor;
 
-    //apply the old canvas to the new one
-    newContext.scale(inverse_downsample_factor, inverse_downsample_factor);
-    newContext.drawImage(oldCanvas, 0, 0);
-    return newCanvas;
+  //apply the old canvas to the new one
+  newContext.scale(inverse_downsample_factor, inverse_downsample_factor);
+  newContext.drawImage(oldCanvas, 0, 0);
+  return newCanvas;
 }
 
 function createAction(name, frame){
-    return {action:name, frame_id: frame};
+  return {action:name, frame_id: frame};
 }
 //api outwards towards game
 function runBot(){
-    //only run bot if you are playing.
-    if (Runner.instance_.playing) {
-        //filter events in previous frames that are no longer valid.
-        actions = actions.filter(function(action){
-            return action.frame_id >= frame_id;
-        });
+  //only run bot if you are playing.
+  if (Runner.instance_.playing) {
+    //filter events in previous frames that are no longer valid.
+    actions = actions.filter(function(action){
+      return action.frame_id >= frame_id;
+    });
 
-        //execute actions
-        let tRex = Runner.instance_.tRex;
-        if(actions.length > 0 && frame_id === actions[0].frame_id){
+    //execute actions
+    let tRex = Runner.instance_.tRex;
+    if(actions.length > 0 && frame_id === actions[0].frame_id){
 
-            //picks the first action on this frame. Thus first action posted is executed for a frame. Others are ignored.
-            let this_action = actions[0].action;
+      //picks the first action on this frame. Thus first action posted is executed for a frame. Others are ignored.
+      let this_action = actions[0].action;
 
-            if (tRex.jumping){
-                console.log('not able to jump on frame: ' +  frame_id + ' because dino is jumping');
-                return;
-            }
+      if (tRex.jumping){
+        console.log('not able to jump on frame: ' +  frame_id + ' because dino is jumping');
+        return;
+      }
 
-            if (this_action === 'jump') {
-                if (tRex.ducking){
-                    tRex.setDuck(false);
-                }
-                tRex.startJump(Runner.instance_.currentSpeed);
-            } else if (this_action === 'duck') {
-                if (tRex.ducking){
-                    tRex.setDuck(false);
-                }else{
-                    tRex.setDuck(true);
-                }
-            } else {
-                console.log('ineligable command: ' + this_action + ' on frame: ' +  frame_id);
-            }
+      if (this_action === 'jump') {
+
+        if (tRex.ducking){
+          tRex.setDuck(false);
         }
-
-        //emit frame as base64image
-        let canvas = copyCanvasWithRescale(document.getElementById('canvasId'), downsamplingNumber).toDataURL();
-        socket.emit('frame', {frame_id: frame_id, frame_data: canvas});
-
-        let state = 'RUNNING';
-        //emit gamestate as custom format
-        if (Runner.instance_.crashed){
-            state = 'CRASHED';
-        } else if (tRex.jumping){
-            state = 'JUMPING';
-        } else if (tRex.ducking){
-            state = 'DUCKING';
+        tRex.startJump(Runner.instance_.currentSpeed);
+      } else if (this_action === 'duck') {
+        if (tRex.ducking){
+          tRex.setDuck(false);
+        }else{
+          tRex.setDuck(true);
         }
-        socket.emit('state', {frame_id: frame_id, status: state, obstacles: Runner.instance_.horizon.obstacles, score: Runner.instance_.distanceMeter.getActualDistance(Math.ceil(Runner.instance_.distanceRan)), high_score: Runner.instance_.distanceMeter.getActualDistance(Math.ceil(Runner.instance_.highestScore))});
-
-        //increment frame count
-        frame_id++;
-    } else if (Runner.instance_.crashed){
-        socket.emit('state', {frame_id: frame_id++, status: "CRASHED", obstacles: Runner.instance_.horizon.obstacles, score: Runner.instance_.distanceMeter.getActualDistance(Math.ceil(Runner.instance_.distanceRan)), high_score: Runner.instance_.distanceMeter.getActualDistance(Math.ceil(Runner.instance_.highestScore))});
+      } else {
+        console.log('ineligable command: ' + this_action + ' on frame: ' +  frame_id);
+      }
     }
+
+    //emit frame as base64image
+    let canvas = copyCanvasWithRescale(document.getElementById('canvasId'), downsamplingNumber).toDataURL();
+    socket.emit('frame', {frame_id: frame_id, frame_data: canvas});
+
+    let state = 'RUNNING';
+    //emit gamestate as custom format
+    if (Runner.instance_.crashed){
+      state = 'CRASHED';
+    } else if (tRex.jumping){
+      state = 'JUMPING';
+    } else if (tRex.ducking){
+      state = 'DUCKING';
+    }
+    socket.emit('state', {frame_id: frame_id, status: state, obstacles: Runner.instance_.horizon.obstacles, score: Runner.instance_.distanceMeter.getActualDistance(Math.ceil(Runner.instance_.distanceRan)), high_score: Runner.instance_.distanceMeter.getActualDistance(Math.ceil(Runner.instance_.highestScore))});
+
+    //increment frame count
+    frame_id++;
+  } else if (Runner.instance_.crashed){
+    if (!is_emitted_gameover){
+      socket.emit('state', {frame_id: frame_id++, status: "CRASHED", obstacles: Runner.instance_.horizon.obstacles, score: Runner.instance_.distanceMeter.getActualDistance(Math.ceil(Runner.instance_.distanceRan)), high_score: Runner.instance_.distanceMeter.getActualDistance(Math.ceil(Runner.instance_.highestScore))});
+      is_emitted_gameover = true;
+    }
+  }
 }
 
 //instance variables
@@ -91,30 +96,31 @@ const downsamplingNumber = 4;
 
 //socket.io bindings
 socket.on('jump', function (frame_id) {
-    console.log("jump added on frame: " + frame_id);
-    actions.push(createAction('jump', frame_id))
+  console.log("jump added on frame: " + frame_id);
+  actions.push(createAction('jump', frame_id))
 });
 
 socket.on('duck', function (frame_id) {
-    console.log("duck added on frame: " + frame_id);
-    actions.push(createAction('duck', frame_id))
+  console.log("duck added on frame: " + frame_id);
+  actions.push(createAction('duck', frame_id))
 });
 
 socket.on('start', function (placeholder) {
-    //placeholder is purposefully not used
-    function resetSettings() {
-        Runner.instance_.tRex.startJump(0);
-        Runner.instance_.update();
+  is_emitted_gameover = false;
+  //placeholder is purposefully not used
+  function resetSettings() {
+    Runner.instance_.tRex.startJump(0);
+    Runner.instance_.update();
+  }
+  if(!Runner.instance_.playing){
+    //if restart or start first time
+    if (Runner.instance_.crashed){
+      Runner.instance_.restart();
+    }else {
+      Runner.instance_.play();
     }
-    if(!Runner.instance_.playing){
-      //if restart or start first time
-      if (Runner.instance_.crashed){
-        Runner.instance_.restart();
-      }else {
-        Runner.instance_.play();
-      }
-      resetSettings();
-    }
+    resetSettings();
+  }
 });
 
 
